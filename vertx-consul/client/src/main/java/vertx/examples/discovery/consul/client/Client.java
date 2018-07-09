86e34cde670b510977889a372e7f1333dea91293
Gary <chengld@yahoo.com>
2018-07-08 21:58:55 -0400

    Refactoring

--- vertx-consul/client/src/main/java/vertx/examples/discovery/consul/client/Client.java
+++ vertx-consul/client/src/main/java/vertx/examples/discovery/consul/client/Client.java
@@ -8,24 +8,27 @@ import org.slf4j.Logger;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
+/**
+ * The client class which find service from consul and call the service
+ *
+ * @author Gary Cheng
+ */
 public class Client {
     private static final Logger logger = getLogger(Client.class);
     private static final String SERVICE_NAME = "MyService";
-
-    private Vertx vertx;
-    private ConsulClient consulClient;
 
     public static void main(String[] args) {
         new Client().start();
     }
 
     private void start() {
-        this.vertx = Vertx.vertx();
-        this.consulClient = ConsulClient.create(this.vertx);
-        vertx.setPeriodic(3000, id -> this.findService().subscribe(this::sendRequest, ex -> logger.error(ex.getMessage())));
+        Vertx vertx = Vertx.vertx();
+        ConsulClient consulClient = ConsulClient.create(vertx);
+        vertx.setPeriodic(3000, id -> this.findService(consulClient)
+                .subscribe(service -> this.sendRequest(vertx, service), ex -> logger.error(ex.getMessage())));
     }
 
-    private Single<Service> findService() {
+    private Single<Service> findService(ConsulClient consulClient) {
         return Single.create(emitter ->
                 consulClient.rxCatalogServiceNodes(SERVICE_NAME)
                         .subscribe(serviceList -> {
@@ -37,7 +40,7 @@ public class Client {
                         }, emitter::onError));
     }
 
-    private void sendRequest(Service service) {
+    private void sendRequest(Vertx vertx, Service service) {
         vertx.createHttpClient().getNow(service.getPort(), service.getAddress(), "/", resp -> {
             logger.debug("Got service response {}", resp.statusCode());
             resp.bodyHandler(body -> logger.debug("body:{}", body.toString()));
--- vertx-consul/service/src/main/java/vertx/examples/discovery/consul/ServiceVerticle.java
+++ vertx-consul/service/src/main/java/vertx/examples/discovery/consul/ServiceVerticle.java
@@ -4,8 +4,11 @@ import io.reactivex.Completable;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.consul.CheckOptions;
+import io.vertx.ext.consul.ConsulClientOptions;
 import io.vertx.ext.consul.ServiceOptions;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.core.http.HttpServerRequest;
 import io.vertx.reactivex.ext.consul.ConsulClient;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
@@ -13,7 +16,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-import java.util.UUID;
 
 /**
  * The Service verticle on HTTP server and registered on consul server
@@ -22,59 +24,76 @@ import java.util.UUID;
  */
 public class ServiceVerticle extends AbstractVerticle {
     private static final Logger logger = LoggerFactory.getLogger(ServiceVerticle.class);
-    private static final String KEY_SERVICE_NAME = "ServiceName";
+    private static final int SC_SERVICE_UNAVAILABLE = 503;
+    private static final String KEY_SERVICE = "service";
+    private static final String KEY_CONSUL = "consul";
+    private static final String KEY_NAME = "name";
     private static final String KEY_HOST = "host";
     private static final String KEY_PORT = "port";
+    private static final String KEY_HEALTH_CHECK_URL = "healthCheck";
 
-    private static final String HEALTH_CHECK_URL = "/healthCheck";
     private ConsulClient consulClient;
-    private String serviceId;
+    private String registeredServiceId;
 
     @Override
     public void start(Future<Void> startFuture) {
-        JsonObject config = this.config();
-        int port = config.getInteger(KEY_PORT);
-        this.consulClient = ConsulClient.create(vertx);
+        logger.debug("Starting verticle");
+        JsonObject serviceConfig = this.config().getJsonObject(KEY_SERVICE);
+        JsonObject consulConfig = this.config().getJsonObject(KEY_CONSUL);
+
+        ConsulClientOptions options = new ConsulClientOptions()
+                .setHost(consulConfig.getString(KEY_HOST, "127.0.0.1"))
+                .setPort(consulConfig.getInteger(KEY_PORT, 8500));
+        this.consulClient = ConsulClient.create(vertx, options);
+
+        String serviceName = serviceConfig.getString(KEY_NAME);
+        String serviceHost = serviceConfig.getString(KEY_HOST, "localhost");
+        Integer servicePort = serviceConfig.getInteger(KEY_PORT, 8080);
+        String healthCheckURL = serviceConfig.getString(KEY_HEALTH_CHECK_URL);
+
         Router router = Router.router(vertx);
-        router.get(HEALTH_CHECK_URL).handler(this::healthCheckHandler);
+        router.get(healthCheckURL).handler(this::healthCheckHandler);
         router.route("/").handler(this::serviceHandler);
-        vertx.createHttpServer()
-                .requestHandler(router::accept)
-                .rxListen(port)
-                .doOnSuccess(s -> logger.debug("HTTP server started"))
-                .flatMapCompletable(s -> this.registerService())
-                .doOnComplete(() -> logger.debug("Register service completed"))
-                .doOnError(startFuture::fail)
-                .subscribe(startFuture::complete);
+        HttpServer server = vertx.createHttpServer();
+        server.requestStream()
+                .toFlowable()
+                .map(HttpServerRequest::pause)
+                .onBackpressureDrop(req -> req.response().setStatusCode(SC_SERVICE_UNAVAILABLE).end())
+                .subscribe(req -> {
+                    logger.debug("Received service request");
+                    req.resume();
+                    router.accept(req);
+                });
+        server.rxListen(servicePort)
+                .doAfterSuccess(s -> logger.debug("http server started on port {}", servicePort))
+                .flatMapCompletable(s -> this.registerService(serviceName, serviceHost, servicePort, healthCheckURL))
+                .doOnComplete(() -> logger.debug("Service published"))
+                .subscribe(startFuture::complete, startFuture::fail);
     }
 
     @Override
     public void stop(Future<Void> stopFuture) {
         logger.debug("stopping verticle");
-        if (null != this.serviceId) {
-            this.consulClient.rxDeregisterService(this.serviceId)
+        if (null != this.registeredServiceId) {
+            this.consulClient.rxDeregisterService(this.registeredServiceId)
                     .doOnComplete(() -> logger.debug("Service de-registered"))
                     .subscribe(stopFuture::complete);
         }
     }
 
-    private Completable registerService() {
+    private Completable registerService(String name, String host, Integer port, String healthCheck) {
         logger.debug("Register service");
-        JsonObject config = this.config();
-        String serviceName = config.getString(KEY_SERVICE_NAME);
-        String host = config.getString(KEY_HOST, "localhost");
-        Integer port = config.getInteger(KEY_PORT, 8080);
-        this.serviceId = UUID.randomUUID().toString();
-        ServiceOptions options = new ServiceOptions().setName(serviceName)
-                .setId(serviceId)
+        ServiceOptions options = new ServiceOptions().setName(name)
+                .setId(name)
                 .setTags(Arrays.asList("http-endpoint"))
                 .setAddress(host)
                 .setPort(port)
                 .setCheckOptions(new CheckOptions()
-                        .setHttp("http://" + host + ":" + port + HEALTH_CHECK_URL)
+                        .setHttp("http://" + host + ":" + port + healthCheck)
                         .setInterval("30s")
                         .setDeregisterAfter("30m"));
-        return this.consulClient.rxRegisterService(options);
+        return this.consulClient.rxRegisterService(options)
+                .doOnComplete(() -> this.registeredServiceId = name);
     }
 
     private void serviceHandler(RoutingContext context) {
