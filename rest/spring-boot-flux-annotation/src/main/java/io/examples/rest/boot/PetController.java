e4ccf878d24a1c518c7bcba3be28f2ab18cb08b6
Gary <chengld@yahoo.com>
2018-08-28 20:49:31 -0400

    Add Spring-boot web flux example

--- rest/spring-boot-flux-annotation/src/main/java/io/examples/rest/boot/PetController.java
+++ rest/spring-boot-flux-annotation/src/main/java/io/examples/rest/boot/PetController.java
@@ -3,18 +3,14 @@ package io.examples.rest.boot;
 import io.examples.common.domain.ApiResponse;
 import io.examples.common.domain.Product;
 import io.examples.common.repository.adapters.ReactorProductRepositoryAdapter;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * @author Gary Cheng
@@ -52,29 +48,27 @@ public class PetController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity> update(@PathVariable("id") Integer id, @RequestBody Product product) {
+    public Mono<ResponseEntity<ApiResponse>> update(@PathVariable("id") Integer id, @RequestBody Product product) {
         return productRepository.getProductById(id)
                 .flatMap(p -> {
                     product.setId(p.getId());
                     return productRepository.updateProduct(product);
                 })
-                .map(p -> this.apiResponse(1, "Update pet successfully"))
+                .map(p -> ApiResponse.message(1, "Update pet successfully"))
+                .map(ResponseEntity::ok)
                 .defaultIfEmpty(this.petNotFound());
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public Mono<ResponseEntity> delete(@PathVariable("id") Integer id) {
+    public Mono<ResponseEntity<ApiResponse>> delete(@PathVariable("id") Integer id) {
         return productRepository.getProductById(id)
                 .flatMap(p -> productRepository.deleteProduct(id))
-                .map(i -> this.apiResponse(2, "Delete pet successfully"))
+                .map(deleted -> ApiResponse.message(2, "Delete pet successfully"))
+                .map(ResponseEntity::ok)
                 .defaultIfEmpty(this.petNotFound());
     }
 
     private ResponseEntity<ApiResponse> petNotFound() {
         return new ResponseEntity<>(ApiResponse.error(101, "Pet not found"), HttpStatus.NOT_FOUND);
-    }
-
-    private ResponseEntity apiResponse(int code, String message) {
-        return ResponseEntity.ok(ApiResponse.message(code, message));
     }
 }
