ea82a57e8420ab17bd212f60c8fb3355c85c8434
Gary <chengld@yahoo.com>
2018-11-22 21:42:34 -0500

    CQRS

--- cqrs/cqrs-axon/src/main/java/io/examples/cqrs/petstore/queryobject/ProductQueryObject.java
+++ cqrs/cqrs-axon/src/main/java/io/examples/cqrs/petstore/queryobject/ProductQueryObject.java
@@ -1,9 +1,5 @@
 package io.examples.cqrs.petstore.queryobject;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -11,12 +7,43 @@ import javax.persistence.Id;
  * @author Gary Cheng
  */
 @Entity(name = "Product")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ProductQueryObject {
+
+    public ProductQueryObject() {
+    }
+
+    public ProductQueryObject(String productId, String name, String category) {
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+    }
+
     @Id
     private String productId;
     private String name;
     private String category;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
