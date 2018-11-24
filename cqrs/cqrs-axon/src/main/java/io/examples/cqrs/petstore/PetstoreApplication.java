57b6950a2ffba6ca9096203157f61827081111b7
Gary <chengld@yahoo.com>
2018-11-23 21:53:47 -0500

    CRQS - Axon

--- cqrs/cqrs-axon/src/main/java/io/examples/cqrs/petstore/PetstoreApplication.java
+++ cqrs/cqrs-axon/src/main/java/io/examples/cqrs/petstore/PetstoreApplication.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class PetstoreApplication {
 
     public static void main(String[] args) {
