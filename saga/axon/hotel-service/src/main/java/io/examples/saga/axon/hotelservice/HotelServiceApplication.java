f8bf84d15c7f6250741e1b1a691faa10885fe7b0
Gary <chengld@yahoo.com>
2018-11-29 22:09:51 -0500

    Axon saga example

---
+++ saga/axon/hotel-service/src/main/java/io/examples/saga/axon/hotelservice/HotelServiceApplication.java
@@ -0,0 +1,12 @@
+package io.examples.saga.axon.hotelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServiceApplication.class, args);
+	}
+}
---
+++ saga/axon/hotel-service/src/test/java/io/examples/saga/axon/hotelservice/HotelServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.examples.saga.axon.hotelservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
