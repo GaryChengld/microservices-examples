29a2fe0109ca5db0d9bd3b840c69311c344b3c5b
Gary <chengld@yahoo.com>
2018-12-05 20:27:14 -0500

    Refactoring

--- saga/axon/travel-service/src/main/java/io/examples/saga/axon/travel/TravelApp.java
+++ saga/axon/travel-service/src/main/java/io/examples/saga/axon/travel/TravelApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravelApplication {
+public class TravelApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TravelApplication.class, args);
+		SpringApplication.run(TravelApp.class, args);
 	}
 }
